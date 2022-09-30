650dfeb0813cc7309fd2a234c2e5be17cdbf2b7a
parkhohyun <haor3@hanmail.net>
2022-09-30 15:51:48 +0900

    feat: Add PreViewCell

---
+++ K-Keyboard/Common/CustomView/CollectionVeiwCell/PreViewItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  PreViewItemCell.swift
+//  K-Keyboard
+//
+//  Created by 박호현 on 2022/09/30.
+//
+
+import UIKit
+
+class PreViewItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ K-Keyboard/Common/CustomView/PreViewCollectionCell/PreViewItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  PreViewItemCell.swift
+//  K-Keyboard
+//
+//  Created by 박호현 on 2022/09/30.
+//
+
+import UIKit
+
+class PreViewItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
+
