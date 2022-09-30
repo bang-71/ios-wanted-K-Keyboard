565351b8f7c788d537da68b702609adca52af79f
parkhohyun <haor3@hanmail.net>
2022-09-30 16:38:32 +0900

    feat: Add AboutThemeCell

---
+++ K-Keyboard/Common/CustomView/AboutThemeCell/CollectionCell/AboutThemeItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  AboutThemeItemCell.swift
+//  K-Keyboard
+//
+//  Created by 박호현 on 2022/09/30.
+//
+
+import UIKit
+
+class AboutThemeItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ K-Keyboard/Common/CustomView/AboutThemeCell/TableViewCell/AboutThemeCell.swift
@@ -0,0 +1,23 @@
+//
+//  AboutThemeCell.swift
+//  K-Keyboard
+//
+//  Created by 박호현 on 2022/09/30.
+//
+
+import UIKit
+
+class AboutThemeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
